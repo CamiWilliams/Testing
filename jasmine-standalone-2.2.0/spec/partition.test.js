01496214edc68670cbb9dab97478baacac3599c9
Kien Nguyen <kiennt.sic@gmail.com>
2015-04-17 18:36:26 -0500

    first test partition
    first test partition

--- jasmine-standalone-2.2.0/spec/partition.test.js
+++ jasmine-standalone-2.2.0/spec/partition.test.js
@@ -1,3 +1,7 @@
 describe("Range", function() {
-  
+  it("when start < 0, end >=0, step === undefined", function() {
+    var range = Immutable.Range(-5, 5);
+    expect(range.toString()).toBe("Range [ -5 ... 5 ]");
+
+  });
 })
