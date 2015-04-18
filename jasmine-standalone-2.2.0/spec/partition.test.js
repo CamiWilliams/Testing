dc90bc079289d56840670f886affea7185fb5aca
Zach Plata <zplata@iastate.edu>
2015-04-17 19:20:02 -0500

    Fixed Input Space
    Testttsss doneeee

--- jasmine-standalone-2.2.0/spec/partition.test.js
+++ jasmine-standalone-2.2.0/spec/partition.test.js
@@ -1,29 +1,29 @@
 describe("Range", function() {
   it("when start undefined, end undefined, step undefined", function() {
-  	var range = Immutable.Range(,,);
+  	var range = Immutable.Range(undefined,undefined,undefined);
   	expect(range.toString()).toBe("Range [ 0..." + Infinity + " ]");
   	expect(range.includes(4)).toBeTruthy();
-  	expect(range.includes(-4)).toBeFalsey():
+  	expect(range.includes(-4)).toBeFalsy();
   });
 
   it("when start undefined, end < 0, step === 0", function() {
   	var range;
   	expect(function() {
-  		range = Immutable.Range(, -5, 0);
+  		range = Immutable.Range(undefined, -5, 0);
   	}).toThrow(new Error('Cannot step a Range by 0'));
   });
 
   it("when start undefined, end >= 0, step !== 0", function() {
-  	var range = Immutable.Range(, 10, 2);
+  	var range = Immutable.Range(undefined, 10, 2);
   	expect(range.toString()).toBe("Range [ 0...10 by 2 ]");
   	expect(range.includes(4)).toBeTruthy();
-  	expect(range.includes(12)).toBeFalsey();
+  	expect(range.includes(12)).toBeFalsy();
   });
 
   it("when start < 0, end undefined, step === 0", function() {
   	var range;
   	expect(function() {
-  		range = Immutable.Range(- 10,, 0);
+  		range = Immutable.Range(- 10,undefined, 0);
   	}).toThrow(new Error('Cannot step a Range by 0'));
   });
 
@@ -31,11 +31,11 @@ describe("Range", function() {
   	var range = Immutable.Range(-10, -2, 2);
   	expect(range.toString()).toBe("Range [ -10...-2 by 2 ]");
   	expect(range.includes(-6)).toBeTruthy();
-  	expect(range.includes(0)).toBeFalsey();
+  	expect(range.includes(0)).toBeFalsy();
   });
 
   it("when start < 0, end >=0, step === undefined", function() {
-    var range = Immutable.Range(-5, 5,);
+    var range = Immutable.Range(-5, 5,undefined);
     expect(range.toString()).toBe("Range [ -5...5 ]");
 
   });
