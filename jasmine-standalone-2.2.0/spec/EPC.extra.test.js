53be03126cdfc421be7e8988de57b138dd28519d
Zach Plata <zplata@iastate.edu>
2015-04-28 16:26:52 -0500

    kjd
    skldjfs

--- jasmine-standalone-2.2.0/spec/EPC.extra.test.js
+++ jasmine-standalone-2.2.0/spec/EPC.extra.test.js
@@ -1,10 +1,11 @@
 describe('EPC-extra', function() {
+  // EPC: [1,2,8]
   it("one of the object is null", function() {
     var val = Immutable.is(null, 2);
     expect(val).toBeFalsy();
   })
-  
-  it("function values", function() {
+  // EPC: [1,2,3], [2,3,4], [3,4,9]
+  it("2 functions, same value", function() {
   	var func = function(a, b){
   		return a+b;
   	};
@@ -14,7 +15,7 @@ describe('EPC-extra', function() {
   	expect(Immutable.is(func,func2).toBeTruthy();
   });
 
-  it("function values", function() {
+  it("1 function as both parameters", function() {
   	var func = function(a, b){
   		return a+b;
   	};
