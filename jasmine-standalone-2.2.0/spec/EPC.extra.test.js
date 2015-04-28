d0361cb45f8da70e7a41995c6c4c284e28934f49
Zach Plata <zplata@iastate.edu>
2015-04-28 16:52:25 -0500

    More EPC tests
    sdkfjsdf

--- jasmine-standalone-2.2.0/spec/EPC.extra.test.js
+++ jasmine-standalone-2.2.0/spec/EPC.extra.test.js
@@ -1,25 +1,29 @@
 describe('EPC-extra', function() {
+
   // EPC: [1,2,8]
   it("one of the object is null", function() {
     var val = Immutable.is(null, 2);
     expect(val).toBeFalsy();
-  })
+  });
+
   // EPC: [1,2,3], [2,3,4], [3,4,9]
-  it("2 functions, same value", function() {
-  	var func = function(a, b){
-  		return a+b;
-  	};
-  	var func2 = function(a, b){
-  		return a+b;
-  	}
-  	expect(Immutable.is(func,func2).toBeTruthy();
+  it("2 Equal Ranges", function() {
+  	var ran = Immutable.Range(1, 10, 1);
+  	var ran2 = Immutable.Range(1, 10, 1);
+  	expect(Immutable.is(ran, ran2)).toBeTruthy();
   });
 
-  it("1 function as both parameters", function() {
-  	var func = function(a, b){
-  		return a+b;
-  	};
-  	expect(Immutable.is(func,func).toBeTruthy();
+  // EPC: [1,2,3], [2,3,4], [3,4,5], [4,5,10]
+  it("1 valid Range, 1 undefined", function() {
+  	var ran = Immutable.Range(1, 10, 1);
+  	var ran2 = Immutable.Range(1, undefined, 1);
+  	expect(Immutable.is(ran, ran2)).toBeFalsy();
   });
 
-})
+  // EPC: [1,2,3], [2,3,6], [3,6,8]
+  it("2 values, not equal", function() {
+  	var a = 1; var b = 2;
+  	expect(Immutable.is(a, b)).toBeFalsy();
+  });
+
+});
