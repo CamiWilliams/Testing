786a8d3655d30fa14968c18924d1ebbdbb83e771
Kien Nguyen <kiennt.sic@gmail.com>
2015-04-28 15:17:33 -0500

    first extra test
    first extra test

--- jasmine-standalone-2.2.0/spec/partition.extra.test.js
+++ jasmine-standalone-2.2.0/spec/partition.extra.test.js
@@ -1,3 +1,11 @@
 describe("partition extra", function() {
-  
+  // wholeSlice function:
+  // parameters: begin, end, size
+  // begin: undefined, <0, 0, >0 // b1, b2, b3, b4
+  // end: undefined, <0, >=0 // e1, e2, e3
+  // size: undefined, >=0 // s1, s2
+  it("begin == undefined, end == undefined, size == undefined", function() {
+    var val = Immutable.wholeSlice(undefined, undefined, undefined);
+    expect(val).toBeFalsy();
+  })
 })
