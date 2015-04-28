67de74f65ae3908520f0ead31d107ff2d3561abd
Kien Nguyen <kiennt.sic@gmail.com>
2015-04-28 15:32:17 -0500

    finish extra partition
    finish extra partition

--- jasmine-standalone-2.2.0/spec/partition.extra.test.js
+++ jasmine-standalone-2.2.0/spec/partition.extra.test.js
@@ -7,5 +7,60 @@ describe("partition extra", function() {
   it("begin == undefined, end == undefined, size == undefined", function() {
     var val = Immutable.wholeSlice(undefined, undefined, undefined);
     expect(val).toBeFalsy();
+  });
+
+  it("begin == undefined, end < 0, size >= 0", function() {
+    var val = Immutable.wholeSlice(undefined, -5, 4);
+    expect(val).toBeFalsy();
+  });
+
+  it("begin == undefined, end >= 0, size == undefined", function() {
+    var val = Immutable.wholeSlice(undefined, 5, undefined);
+    expect(val).toBeFalsy();
+  });
+
+  it("begin < 0, end == undefined, size >= 0", function() {
+    var val = Immutable.wholeSlice(-6, undefined, 5);
+    expect(val).toBeTruthy();
   })
+
+  it("begin < 0, end < 0, size == undefined", function() {
+    var val = Immutable.wholeSlice(-4, -5, undefined);
+    expect(val).toBeFalsy();
+  });
+
+  it("begin < 0, end >= 0, size >= 0", function() {
+    var val = Immutable.wholeSlice(-5, 5, 5);
+    expect(val).toBeTruthy();
+  });
+
+  it("begin == 0, end == undefined, size == undefined", function() {
+    var val = Immutable.wholeSlice(0, undefined, undefined);
+    expect(val).toBeTruthy();
+  });
+
+  it("begin == 0, end < 0, size >= 0", function() {
+    var val = Immutable.wholeSlice(0, -5, 5);
+    expect(val).toBeFalsy();
+  });
+
+  it("begin == 0, end >= 0, size == undefined", function() {
+    var val = Immutable.wholeSlice(0, 5, undefined);
+    expect(val).toBeFalsy();
+  });
+
+  it("begin > 0, end == undefined, size >= 0", function() {
+    var val = Immutable.wholeSlice(4, undefined, 2);
+    expect(val).toBeFalsy();
+  });
+
+  it("begin > 0, end < 0, size == undefined", function() {
+    var val = Immutable.wholeSlice(4, -5, undefined);
+    expect(val).toBeFalsy();
+  });
+
+  it("begin > 0, end >= 0, size >= 0", function() {
+    var val = Immutable.wholeSlice(5, 5, 5);
+    expect(val).toBeFalsy();
+  });
 })
